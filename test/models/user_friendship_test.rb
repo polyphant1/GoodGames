f6d48f9b2eaac92251c53718897c8b515d5d7a04
polyphant1 <chrisl1991@hotmail.co.uk>
2015-01-02 21:54:40 +0000

    updated user_friendship model to allow emails to be sent to friends, and to add two records to the user_friendship table

--- test/models/user_friendship_test.rb
+++ test/models/user_friendship_test.rb
@@ -1,18 +1,78 @@
 require 'test_helper'
 
 class UserFriendshipTest < ActiveSupport::TestCase
-    should belong_to(:user)
-    should belong_to(:friend)
+  should belong_to(:user)
+  should belong_to(:friend)
     
-    test "that creating a friendship works without raising an exception" do
-        assert_nothing_raised do
-            UserFriendship.create user: users(:chris), friend: users(:mike)
-        end        
+  test "that creating a friendship works without raising an exception" do
+    assert_nothing_raised do
+      UserFriendship.create user: users(:chris), friend: users(:mike)
+    end        
+  end
+  
+  test "that creating a friendship based on user id and friend id works" do
+    UserFriendship.create user_id: users(:chris).id, friend_id: users(:mike).id
+    assert users(:chris).pending_friends.include?(users(:mike))
+  end
+  
+  context "a new instance" do
+    setup do
+      @user_friendship = UserFriendship.new user: users(:chris), friend: users(:mike)
     end
     
-    test "that creating a friendship based on user id and friend id works" do
-        UserFriendship.create user_id: users(:chris).id, friend_id: users(:mike).id
-        assert users(:chris).friends.include?(users(:mike))
+    should "have a pending state" do
+      assert_equal 'pending', @user_friendship.state  
     end
+  end
+  
+  context "#send_request_email" do
+    setup do
+      @user_friendship = UserFriendship.create user: users(:chris), friend: users(:mike)
+    end
+    
+    should "send an email" do
+      assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+        @user_friendship.send_request_email
+      end
+    end
+  end
+  
+  context "#accept!" do
+    setup do
+      @user_friendship = UserFriendship.create user: users(:chris), friend: users(:mike)
+    end
+    
+    should "set the state to accepted" do
+      @user_friendship.accept!
+      assert_equal "accepted", @user_friendship.state
+    end
+    
+    should "send an acecptance email" do
+      assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+        @user_friendship.accept!
+      end
+    end
+    
+    should "include friend in list of friends" do
+      @user_friendship.accept!
+      users(:chris).friends.reload
+      assert users(:chris).friends.include?(users(:mike))
+    end
+    
+  end
+  
+  context ".request" do
+    should "create two user friendships" do
+      assert_difference 'UserFriendship.count', 2 do
+        UserFriendship.request(users(:chris), users(:mike))
+      end
+    end
+    
+    should "send a friend request email" do
+      assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+        UserFriendship.request(users(:chris), users(:mike))
+      end
+    end
+  end
   
 end
